20455e9266fa60d4527dc01208350b01e9f84f49
lowpolyfox <alan.maldonado.lara1@gmail.com>
2024-02-18 23:29:41 -0600

    Add date to Post schema and render date on homepage and Post layout; fix Post TS errors; rename first post

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const postsCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
+    date: z.date(),
   }),
 });
 
--- src/content/posts/inauguration.md
+++ src/content/posts/inauguration.md
@@ -1,5 +1,6 @@
 ---
 title: Inauguration
+date: 2023-11-10
 ---
 
 Kissed by the wind, the first leaf falls from the tree, ready to feed the soil.
