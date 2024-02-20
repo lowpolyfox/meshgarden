bf989083866f74dd7ce7ecca967d386bc9a304b6
lowpolyfox <alan.maldonado.lara1@gmail.com>
2024-02-20 17:59:00 -0600

    Add Google Analytics with Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.meshgarden.space/",
-  integrations: [tailwind(), sitemap()],
-});
+  site: 'https://www.meshgarden.space/',
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push']
+      }
+    })
+  ]
+})
