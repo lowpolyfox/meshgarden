e730bf3d0d68cb89bc90d6104f77510415bf6bed
lowpolyfox <alan.maldonado.lara1@gmail.com>
2025-01-29 09:59:51 -0600

    improve the dimensions calculation to prevent overflow on large screens; always show a portion of the next photo

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -2,7 +2,10 @@ import type { Asset, Entry } from 'contentful'
 import { useEffect, useRef, useState, type ReactNode } from 'react'
 import type { Post } from '../lib/contentful'
 
-const NAV_WIDTH_IN_PX = 250
+const NAV_WIDTH_IN_PX = 240
+const VERTICAL_PADDING_IN_PX = 40
+const MIN_NEXT_PHOTO_VISIBLE_PORTION_IN_PX = 60
+const DEBOUNCE_TIMEOUT_IN_MS = 100
 interface Props {
   images: Asset[]
   posts: Entry<Post>[]
@@ -10,15 +13,31 @@ interface Props {
 }
 const PhotoGallery = ({ images, posts, children }: Props) => {
   const containerElement = useRef<HTMLDivElement>(null)
-  const [width, setWidth] = useState(0)
-  const [height, setHeight] = useState(0)
+  const [photoWidth, setPhotoWidth] = useState(0)
+  const [photoHeight, setPhotoHeight] = useState(0)
 
-  const calculateDimensions = () => {
+  const calculatePhotoDimensions = () => {
     if (containerElement.current) {
-      const width = window.innerWidth - NAV_WIDTH_IN_PX
-      const height = (width * 2) / 3
-      setHeight(height)
-      setWidth(width)
+      const maxHeight = window.innerHeight - VERTICAL_PADDING_IN_PX
+      const viewportWidth = window.innerWidth
+
+      let width =
+        containerElement.current.offsetWidth -
+        MIN_NEXT_PHOTO_VISIBLE_PORTION_IN_PX
+      let height = (width * 2) / 3
+
+      if (height > maxHeight) {
+        height = maxHeight
+        width = (height * 3) / 2
+      }
+
+      if (width > viewportWidth) {
+        width = viewportWidth
+        height = (width * 2) / 3
+      }
+
+      setPhotoHeight(height)
+      setPhotoWidth(width)
     }
   }
 
@@ -31,67 +50,77 @@ const PhotoGallery = ({ images, posts, children }: Props) => {
   }
 
   useEffect(() => {
-    calculateDimensions()
+    calculatePhotoDimensions()
     const handleResize = debounce(() => {
-      calculateDimensions()
-    }, 100)
-    window.addEventListener('resize', handleResize)
+      calculatePhotoDimensions()
+    }, DEBOUNCE_TIMEOUT_IN_MS)
 
+    window.addEventListener('resize', handleResize)
     return () => {
       window.removeEventListener('resize', handleResize)
     }
   }, [])
 
   return (
-    <div>
-      <div className="flex items-center h-screen">
-        <nav
-          className="h-full bg-white px-4"
-          style={{
-            width: NAV_WIDTH_IN_PX,
-            paddingTop: (window.innerHeight - height) / 2,
-          }}
-        >
-          <h1 className="font-bold mb-4">Analog Photography</h1>
-          {posts.map((post) => {
-            const formattedDate = new Date(post.fields.date as string)
-              .toLocaleDateString('en-US', {
-                month: 'long',
-                year: 'numeric',
-              })
-              .replace(/(\w+)\s(\d+)/, '$1, $2')
+    <div className="flex h-screen">
+      <nav
+        className="min-h-full bg-white px-4"
+        style={{
+          width: NAV_WIDTH_IN_PX,
+          paddingTop: (window.innerHeight - photoHeight) / 2,
+          paddingBottom: (window.innerHeight - photoHeight) / 2,
+        }}
+      >
+        <h1 className="font-bold mb-4">Photography</h1>
+        {posts.map((post) => {
+          const formattedDate = new Date(String(post.fields.date))
+            .toLocaleDateString('en-US', {
+              month: 'long',
+              year: 'numeric',
+            })
+            .replace(/(\w+)\s(\d+)/, '$1, $2')
 
-            return (
-              <a key={post.sys.id} href={`/posts/${post.fields.slug}`}>
-                <span>{post.fields.title as string}</span>
-                <span className="block italic text-xs">{formattedDate}</span>
-              </a>
-            )
-          })}
-        </nav>
-        <section
-          className="mx-auto flex gap-10 overflow-x-scroll "
-          ref={containerElement}
-          style={{
-            width: `${width}px`,
-            height: `${height}px`,
-          }}
-        >
-          {children && <div className="min-w-[200px]">{children}</div>}
-          {images.map((img) => (
-            <div
-              key={img.sys.id}
-              className="relative overflow-hidden h-full min-w-full"
-            >
-              <img
-                src={img.fields.file?.url as string}
-                alt=""
-                className="absolute inset-0 size-full object-contain"
-              />
-            </div>
-          ))}
-        </section>
-      </div>
+          return (
+            <a key={post.sys.id} href={`/posts/${post.fields.slug}`}>
+              <span>{String(post.fields.title)}</span>
+              <span className="block italic text-xs">{formattedDate}</span>
+            </a>
+          )
+        })}
+      </nav>
+      <section
+        className="mx-auto flex gap-3 overflow-x-scroll flex-1 h-full items-center"
+        ref={containerElement}
+      >
+        {children && (
+          <div
+            className="min-w-[360px] h-full "
+            style={{
+              paddingTop: (window.innerHeight - photoHeight) / 2,
+              paddingBottom: (window.innerHeight - photoHeight) / 2,
+            }}
+          >
+            {children}
+          </div>
+        )}
+        {images.map((img) => (
+          <div
+            key={img.sys.id}
+            className="relative overflow-hidden"
+            style={{
+              minWidth: `${photoWidth}px`,
+              width: `${photoWidth}px`,
+              height: `${photoHeight}px`,
+            }}
+          >
+            <img
+              src={String(img.fields.file?.url)}
+              alt=""
+              className="absolute inset-0 size-full object-contain"
+            />
+          </div>
+        ))}
+      </section>
     </div>
   )
 }
